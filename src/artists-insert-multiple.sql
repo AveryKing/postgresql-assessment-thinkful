20afaa861487efc167c871e29066177bc59abe7a
Avery King <avery.n.king@wmich.edu>
2022-03-17 18:43:30 -0400

    tests passing

---
+++ src/artists-insert-multiple.sql
@@ -0,0 +1,23 @@
+insert into artists 
+(artist_name, birthday, birthplace, is_alive)
+values (
+'Wasgfgdggdgfinsky',
+'1866-12-16',
+  'Moscow, Russia',
+  'false'
+),(
+'Wassgfgffggfndinsky',
+'1866-12-16',
+  'Moscow, Russia',
+  'false'
+),(
+'Wassiyrhgtrndinsky',
+'1866-12-16',
+  'Moscow, Russia',
+  'false'
+),(
+'Wastrbggtrsky',
+'1866-12-16',
+  'Moscow, Russia',
+  'false'
+)
